09478230241b598ff47d7ad9d746f650143eb449
Abdulaziz Abukar <abdulaziz.abukar@edu.sait.ca>
2024-05-23 13:21:45 -0600

    QuickSort Algorithm

--- QuickSort.py
+++ QuickSort.py
@@ -1,1 +1,23 @@
-# QuickSort (n log n)
+# QuickSort (n log n) worst case (n^2)
+
+def quick_sort(sequence):
+    length = len(sequence)
+    if length <= 1:
+        return sequence
+    else:
+        pivot = sequence.pop()
+
+    items_greater = []
+    items_lower = []
+
+    for item in sequence:
+        if item > pivot:
+            items_greater.append(item)
+
+        else:
+            items_lower.append(item)
+
+    return quick_sort(items_lower) + [pivot] + quick_sort(items_greater)
+
+
+print(quick_sort([5, 2, 9, 6, 1, 2, 6, 0, 8, 9, 10, 3, 6]))
